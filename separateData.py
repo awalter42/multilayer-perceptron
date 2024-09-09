0ed472e7cfc3e74a0bca32a063de3cb939e1d3dc
Andrea <andrea.walter03@gmail.com>
2024-09-09 13:29:23 +0200

    datas with the program to separate them

---
+++ separateData.py
@@ -0,0 +1,52 @@
+import random
+
+
+def fetchData():
+	try:
+		file = open('data.csv', "r")
+	except:
+		print(f'There has been a problem when fetching the file {file}')
+	tab = []
+	for line in file:
+		tab.append(line)
+	file.close()
+	return tab
+
+
+def splitData(data):
+	length = len(data)
+	trainSize = int(length*0.8)
+	trainValidSize = int(length*0.9)
+	training = data[:trainSize]
+	validation = data[trainSize:trainValidSize]
+	test = data[trainValidSize:]
+
+	return training, validation, test
+
+
+def saveDatas(training, validation, test):
+	try:
+		f = open('trainingData.csv', 'w')
+		for line in training:
+			f.write(line)
+		f.close()
+
+		f = open('validationData.csv', 'w')
+		for line in validation:
+			f.write(line)
+		f.close()
+
+		f = open('testData.csv', 'w')
+		for line in test:
+			f.write(line)
+		f.close()
+	except:
+		print('there has been a problem saving the data')
+
+
+if __name__ == '__main__':
+	data = fetchData()
+	random.shuffle(data)
+	training, validation, test = splitData(data)
+	saveDatas(training, validation, test)
+
