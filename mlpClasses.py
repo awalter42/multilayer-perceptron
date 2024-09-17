02fdcea1d2f96736ffed7c1048752c33f475dc95
Andrea <andrea.walter03@gmail.com>
2024-09-17 15:15:50 +0200

    Not sur about the classes yet

---
+++ mlpClasses.py
@@ -0,0 +1,20 @@
+import sys
+
+
+class Perceptron:
+	pass
+
+
+class Layer:
+	def __init__(self, size, type):
+		self.size = size
+		self.type = type
+		# self.weights
+
+
+class Network:
+
+	def __init__(self, nbLayer, sizeLayers):
+		self.nbLayer = nbLayer
+		self.sizeLayers = sizeLayers
+		
