cb502f3a889ad749edbbb84cc7ac5be956d00da7
Andrea <andrea.walter03@gmail.com>
2024-10-17 13:36:05 +0200

    added Layer class to store the state of each layer

--- mlpClasses.py
+++ mlpClasses.py
@@ -4,14 +4,43 @@ import math
 
 
 
-# class Layer:
+class Layer:
 
-# 	def __init__(self, size, type):
-# 		self.values = []
-# 		self.weights = []
-# 		self.size = size
-# 		self.type = type
-# 		self.next = None
+	def __init__(self, previous=None, output=False):
+		self.isOutput = output
+		self.values = []
+		self.previous = previous
+		self.next = None
+		self.weight = None
+		self.bias = None
+
+
+	def printLayers(self):
+		for w in self.weight:
+			print(w)
+		print()
+		if not self.next.isOutput:
+			self.next.printLayers()
+
+
+	def addValue(self, v):
+		if self.values == []:
+			self.size = len(v)
+		self.values.append(v)
+
+
+	def setNext(self, next):
+		self.next = next
+
+
+	def setWeight(self, weight):
+		self.weight = weight
+
+
+	def setBias(self, bias):
+		self.bias = bias
+
+
 
 
 class Model:
@@ -21,9 +50,28 @@ class Model:
 		self.func = func
 		self.learning_rate = learning_rate
 		self.weights = self.generateWeights(self.layers)
-		self.bias = self.generateBias(layers)
+		self.bias = self.generateBias(self.layers)
 		self.expectedOutputs = []
 		self.predictedOutputs = []
+		self.setupLayers()
+		self.inputLayer.printLayers()
+
+
+	def setupLayers(self):
+		self.inputLayer = Layer()
+		self.inputLayer.setWeight(self.weights[0])
+		self.inputLayer.setBias(self.bias[0])
+		prev = self.inputLayer
+		current = None
+		for i in range(1, len(self.layers) - 1):
+			current = Layer(previous=prev)
+			current.setWeight(self.weights[i])
+			current.setBias(self.bias[i])
+			prev.setNext(current)
+			prev = current
+		self.outputLayer = Layer(previous=prev, output=True)
+		prev.setNext(self.outputLayer)
+
 
 
 	def generateWeights(self, layers):
@@ -35,7 +83,6 @@ class Model:
 				for j in range(layers[i + 1]):
 					weight_li[l].append(round(random.uniform(-10.0, 10.0), 3))
 			weights.append(weight_li)
-		print(weights)
 		return weights
 
 
